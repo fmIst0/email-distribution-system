7c32027c6cbdcb50b55cb6e385f5353e75728f5f
mIst <bogdankharchenko77@gmail.com>
2023-11-04 19:03:04 +0200

    project created

---
+++ src/test/java/com/example/emaildistributionsystem/EmailDistributionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emaildistributionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailDistributionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
